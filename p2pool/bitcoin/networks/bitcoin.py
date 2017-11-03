8108d8e3e531915f4dee468d7cf2d11d02d5b697
Ivan Ivanov <kzv@narod.ru>
2017-11-03 21:31:57 +0000

    Init Marycoin

--- p2pool/bitcoin/networks/bitcoin.py
+++ p2pool/bitcoin/networks/bitcoin.py
@@ -8,21 +8,21 @@ from p2pool.util import pack
 
 
 P2P_PREFIX = 'f9beb4d9'.decode('hex')
-P2P_PORT = 8333
-ADDRESS_VERSION = 0
-RPC_PORT = 8332
+P2P_PORT = 33333
+ADDRESS_VERSION = 50
+RPC_PORT = 33332
 RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            (yield helper.check_genesis_block(bitcoind, '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')) and
+            (yield helper.check_genesis_block(bitcoind, '00000000445999647df11eba1ac9c2fbb52c5c271417292e13b3282d004a3dc3')) and
             not (yield bitcoind.rpc_getinfo())['testnet']
         ))
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//210000
 POW_FUNC = data.hash256
 BLOCK_PERIOD = 600 # s
-SYMBOL = 'BTC'
-CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Bitcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Bitcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.bitcoin'), 'bitcoin.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'https://blockchain.info/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'https://blockchain.info/address/'
-TX_EXPLORER_URL_PREFIX = 'https://blockchain.info/tx/'
+SYMBOL = 'MC'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Marycoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Marycoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.marycoin'), 'marycoin.conf')
+BLOCK_EXPLORER_URL_PREFIX = 'https://mc.multicoins.org/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://mc.multicoins.org/address/'
+TX_EXPLORER_URL_PREFIX = 'https://mc.multicoins.org/tx/'
 SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
 DUMB_SCRYPT_DIFF = 1
-DUST_THRESHOLD = 0.001e8
+DUST_THRESHOLD = 0.00001
