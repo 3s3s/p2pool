ddea7d2d58b822c0bbcf5fecb99ae4e600293508
3s3s <ivanivanovkzv@gmail.com>
2017-11-04 16:23:50 +0000

    Set Marycoin

--- p2pool/networks/bitcoin.py
+++ p2pool/networks/bitcoin.py
@@ -8,8 +8,8 @@ from p2pool.bitcoin import networks
 
 PARENT = networks.nets['bitcoin']
 SHARE_PERIOD = 30 # seconds
-CHAIN_LENGTH = 24*60*60//10 # shares
-REAL_CHAIN_LENGTH = 24*60*60//10 # shares
+CHAIN_LENGTH = 60*60//10 # shares
+REAL_CHAIN_LENGTH = 60*60//10 # shares
 TARGET_LOOKBEHIND = 200 # shares
 SPREAD = 3 # blocks
 IDENTIFIER = 'fc70035c7a81bc6f'.decode('hex')
