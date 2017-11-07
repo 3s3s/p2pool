8a8f66a2ad74919d1af5d7f021483feb6c8279fd
3s3s <ivanivanovkzv@gmail.com>
2017-11-07 07:22:13 +0000

    new config

--- p2pool/networks/bitcoin.py
+++ p2pool/networks/bitcoin.py
@@ -10,18 +10,18 @@ PARENT = networks.nets['bitcoin']
 SHARE_PERIOD = 30 # seconds
 CHAIN_LENGTH = 24*60*60//10 # shares
 REAL_CHAIN_LENGTH = 24*60*60//10 # shares
-TARGET_LOOKBEHIND = 200 # shares
+TARGET_LOOKBEHIND = 20 # shares
 SPREAD = 3 # blocks
-IDENTIFIER = 'fc70035c7a81bc6f'.decode('hex')
-PREFIX = '2472ef181efcd37b'.decode('hex')
+IDENTIFIER = 'fc70035c7a81bc5f'.decode('hex')
+PREFIX = '2472ef181efcd36b'.decode('hex')
 P2P_PORT = 39333
 MIN_TARGET = 0
-MAX_TARGET = 2**256//2**32//1000000 - 1
+MAX_TARGET = 2**256//2**32 - 1
 PERSIST = False
 WORKER_PORT = 39332
 BOOTSTRAP_ADDRS = '162.243.251.161 193.107.236.167 104.131.65.219'.split(' ')
-ANNOUNCE_CHANNEL = '#p2pool'
-VERSION_CHECK = lambda v: None if 100000 <= v else 'Bitcoin version too old. Upgrade to 0.11.2 or newer!' # not a bug. BIP65 support is ensured by SOFTFORKS_REQUIRED
+ANNOUNCE_CHANNEL = '#p2pool-mc'
+VERSION_CHECK = lambda v: None if 100000 <= v else 'Marycoin version too old. Upgrade to 0.11.2 or newer!' # not a bug. BIP65 support is ensured by SOFTFORKS_REQUIRED
 VERSION_WARNING = lambda v: None
 SOFTFORKS_REQUIRED = set(['bip65', 'csv', 'segwit'])
 MINIMUM_PROTOCOL_VERSION = 1600
